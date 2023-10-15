650e58264c33d0b15ad57716862eb2360a668fc6
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2023-10-15 04:09:41 +0300

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1c4f82",
+
+          secondary: "#7d919b",
+
+          accent: "#eb6b47",
+
+          neutral: "#23282f",
+
+          dark: "#212121",
+
+          info: "#0092d6",
+
+          success: "#6cb288",
+
+          warning: "#daad58",
+
+          error: "#ab3d30",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
